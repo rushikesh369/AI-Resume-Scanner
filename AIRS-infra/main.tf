085a6da2c0593410c4d46bbf370266f4e37bed8f
Rushikesh Maragoni <rushikeshmaragoni@gmail.com>
2026-09-24 19:28:44 +0530

    Update main.tf

--- AIRS-infra/main.tf
+++ AIRS-infra/main.tf
@@ -1,15 +1,19 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "bkt-demo-000"
-  location      = "us-central1"
-  project = "airs-509504"
-  force_destroy = true
+  name                     = "bkt-demo-000"
+  location                 = "us-central1"
+  project                  = "airs-509504"
+  force_destroy            = true
   public_access_prevention = "enforced"
+
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "my-bucket2" {
-  name          = "bkt-demo-002"
-  location      = "us-central1"
-  project = "airs-509504"
-  force_destroy = true
+  name                     = "bkt-demo-002"
+  location                 = "us-central1"
+  project                  = "airs-509504"
+  force_destroy            = true
   public_access_prevention = "enforced"
+
+  uniform_bucket_level_access = true
 }
